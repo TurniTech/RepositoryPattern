7ee69a26ceb1860a2486453cd734e9ed12306e6b
TurniTech <turnitechnology@gmail.com>
2021-11-12 22:58:25 -0800

    test commit

--- Src/TurniTech.RepositoryPattern/TurniTech.RepositoryPattern/Controllers/WeatherForecastController.cs
+++ Src/TurniTech.RepositoryPattern/TurniTech.RepositoryPattern/Controllers/WeatherForecastController.cs
@@ -26,7 +26,9 @@ namespace TurniTech.RepositoryPattern.Service.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+#pragma warning disable SecurityIntelliSenseCS // MS Security rules violation
             var rng = new Random();
+#pragma warning restore SecurityIntelliSenseCS // MS Security rules violation
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
